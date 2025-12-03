9571787d20f4cf03ba035643121f9d046db62e44
karthik <shanigaramkarthik@gmail.com>
2025-12-03 12:08:05 +0530

    fixed realtime chatting by reconnecting socket

--- src/services/Socket.js
+++ src/services/Socket.js
@@ -3,9 +3,9 @@ import { url } from "../config/AppConfig";
 
 export const socket = io(url, {
   transports: ["websocket"],
-  reconnection: false,
-  // reconnectionAttempts: Infinity,
-  // reconnectionDelay: 3000,
-  // reconnectionDelayMax: 3000,
-  // timeout: 5000,
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 3000,
+  reconnectionDelayMax: 3000,
+  timeout: 5000,
 });
