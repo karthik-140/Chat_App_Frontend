6fc9acd907c482c72b96e495892a8b089fae2538
karthik <shanigaramkarthik@gmail.com>
2025-12-10 17:25:16 +0530

    revert socket connection

--- src/services/Socket.js
+++ src/services/Socket.js
@@ -1,13 +1,11 @@
 import io from "socket.io-client";
 import { url } from "../config/AppConfig";
 
-// export const socket = io(url, {
-//   transports: ["websocket"],
-//   reconnection: true,
-//   reconnectionAttempts: Infinity,
-//   reconnectionDelay: 3000,
-//   reconnectionDelayMax: 3000,
-//   timeout: 5000,
-// });
-
-export const socket = io.connect(url);
+export const socket = io(url, {
+  transports: ["websocket"],
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 1000,
+  timeout: 5000,
+});
