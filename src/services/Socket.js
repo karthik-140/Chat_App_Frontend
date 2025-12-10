c65dea00b6c20a4e3b519287ebd4b0266d538626
karthik <shanigaramkarthik@gmail.com>
2025-12-10 16:13:57 +0530

    refactored socket connection

--- src/services/Socket.js
+++ src/services/Socket.js
@@ -1,11 +1,13 @@
 import io from "socket.io-client";
 import { url } from "../config/AppConfig";
 
-export const socket = io(url, {
-  transports: ["websocket"],
-  reconnection: true,
-  reconnectionAttempts: Infinity,
-  reconnectionDelay: 3000,
-  reconnectionDelayMax: 3000,
-  timeout: 5000,
-});
+// export const socket = io(url, {
+//   transports: ["websocket"],
+//   reconnection: true,
+//   reconnectionAttempts: Infinity,
+//   reconnectionDelay: 3000,
+//   reconnectionDelayMax: 3000,
+//   timeout: 5000,
+// });
+
+export const socket = io.connect(url);
